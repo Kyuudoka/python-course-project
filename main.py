1d5ea7dc313afb9901d5a8b1b43982bdc5110e4c
Kyuudoka <chrissie.king@kpmg.co.uk>
2023-09-08 17:05:22 +0100

    added in script which runs the applicators based on complexity

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from custom_shapes import draw_triangle, draw_quad
 # Shape applicator functions -these draw a set number of an identical shape in random orientations and positions on the canvas. Energy and intensity apply here
 
 def apply_triangles(palette, energy):
-    for i in range(round(int(energy) * int(complexity) / 5)):  # Energy sets how many times the applicator runs i.e. how many different types of triangle it draws
+    for i in range(round(int(energy) * int(complexity) / 2)):  # Energy sets how many times the applicator runs i.e. how many different types of triangle it draws
         side1, angle, side2, size = gen_triangle_params(mood_intensity)
         iterations = gen_iterations(size)
         relocate_pen(gen_coord(), gen_coord())
@@ -21,7 +21,7 @@ def apply_triangles(palette, energy):
             rand_orient()
 
 def apply_quads(palette, energy):
-    for i in range(round(int(energy) * int(complexity) / 5)):
+    for i in range(round(int(energy) * int(complexity) / 2)):
         side1, angle1, side2, angle2, side3, size = gen_quad_params(mood_intensity)
         iterations = gen_iterations(size)
 
@@ -33,18 +33,24 @@ def apply_quads(palette, energy):
 def apply_spirographs(palette, energy):
     pass
 
-# Master construction functions -the functions which actually generate the moodstamp based on the user input, using the shape applicators. Complexity applies here
 
+# Code which puts everything together
+
+t.speed(400)
+if int(complexity) >= 5:
+    apply_quads(palette, energy)
+    apply_triangles(palette, energy)
+    apply_spirographs(palette, energy)
+elif int(complexity) >= 3:
+    apply_quads(palette, energy)
+    apply_triangles(palette, energy)
+else:
+    apply_triangles(palette, energy)
 
 
 # Test scripts below here
 
-# apply_triangles(palette, energy)
 
-t.speed(250)
-apply_quads(palette, energy)
-apply_triangles(palette, energy)
-apply_spirographs(palette, energy)
 
 # Test scripts above here
 
